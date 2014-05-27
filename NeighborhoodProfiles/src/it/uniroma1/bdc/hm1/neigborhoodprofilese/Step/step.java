19e91446449c691ca90e04272715256ca8b14244
85piccio <85piccio@gmail.com>
2014-05-27 14:54:16 +0200

    prima implementazione step (da testare)

--- NeighborhoodProfiles/src/it/uniroma1/bdc/hm1/neigborhoodprofilese/Step/step.java
+++ NeighborhoodProfiles/src/it/uniroma1/bdc/hm1/neigborhoodprofilese/Step/step.java
@@ -3,9 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package it.uniroma1.bdc.hm1.neigborhoodprofilese.Step;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashSet;
 
 /**
@@ -14,20 +17,31 @@ import java.util.HashSet;
  */
 public class step {
 
-    public static HashSet performance(HashSet kset, String inputFile) {
-        
+    public static HashSet performance(HashSet kset, String inputFile) throws FileNotFoundException, IOException {
+
         HashSet<String> nextSet = new HashSet();
-        
+
+        BufferedReader br = new BufferedReader(new FileReader(inputFile));
+        String line;
+
         //****PER GRAFI DIRETTI
-        //per ogni riga in input
-            //se il primo elemeno è in kset
-                //add secondo elemento in nextSet
+        while ((line = br.readLine()) != null) {//per ogni riga in input
+            // process the line.
+            String part[] = line.split("\\s+");//TODO:checl split
+
+            if (part.length >= 2) {//se il primo elemeno è in kset
+                if (kset.contains(part[0])) {
+                    nextSet.add(part[1]);//add secondo elemento in nextSet
+                }
+            }
+        }
+        br.close();
+
         //****PER GRAFI NON DIRETTI
         //per ogni riga in input
-            //se uno dei due elementi è in kset
-                //add altro elemento in nextSet
-        
+        //se uno dei due elementi è in kset
+        //add altro elemento in nextSet
         return nextSet;
     }
-    
+
 }
