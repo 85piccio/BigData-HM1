8b001231dfe745e699cc7bb29898c830df3a743e
85piccio <85piccio@gmail.com>
2014-05-30 10:42:31 +0200

    add undirectgraph  option

--- NeighborhoodProfiles/src/it/uniroma1/bdc/hm1/neigborhoodprofiles/Step/step.java
+++ NeighborhoodProfiles/src/it/uniroma1/bdc/hm1/neigborhoodprofiles/Step/step.java
@@ -17,7 +17,7 @@ import java.util.HashSet;
  */
 public class step {
 
-    public static HashSet perform(HashSet kset, HashSet visited, String inputFile, Integer bufDim) throws FileNotFoundException, IOException {
+    public static HashSet performDirectGraph(HashSet kset, HashSet visited, String inputFile, Integer bufDim) throws FileNotFoundException, IOException {
 
         HashSet<String> nextSet = new HashSet();
         BufferedReader br;
@@ -44,6 +44,45 @@ public class step {
 
         }
         br.close();
+        return nextSet;
+    }
+    public static HashSet performUndirectGraph(HashSet kset, HashSet visited, String inputFile, Integer bufDim) throws FileNotFoundException, IOException {
+
+        HashSet<String> nextSet = new HashSet();
+        BufferedReader br;
+        if (bufDim < 0) {
+            br = new BufferedReader(new FileReader(inputFile));
+        } else {
+            br = new BufferedReader(new FileReader(inputFile), bufDim);
+        }
+        String line;
+
+        //****PER GRAFI DIRETTI
+        while ((line = br.readLine()) != null) {//per ogni riga in input
+            // process the line.
+            String part[] = line.split("\\s+");
+
+            if (part.length >= 2) {//se il primo elemeno è in kset
+                
+                //verso 1
+                if (kset.contains(part[0])) {
+                    if (!visited.contains(part[1])) {
+                        nextSet.add(part[1]);//add secondo elemento in nextSet
+                        visited.add(part[1]);//add secondo elemento tra i già visitati
+                    }
+                }
+                
+                //verso 2
+                if (kset.contains(part[1])) {
+                    if (!visited.contains(part[0])) {
+                        nextSet.add(part[0]);//add secondo elemento in nextSet
+                        visited.add(part[0]);//add secondo elemento tra i già visitati
+                    }
+                }
+            }
+
+        }
+        br.close();
 
         //****PER GRAFI NON DIRETTI
         //per ogni riga in input
@@ -51,5 +90,6 @@ public class step {
         //add altro elemento in nextSet
         return nextSet;
     }
+
 
 }
--- NeighborhoodProfiles/src/it/uniroma1/bdc/hm1/neighborhoodprofiles/NeighborhoodProfiles.java
+++ NeighborhoodProfiles/src/it/uniroma1/bdc/hm1/neighborhoodprofiles/NeighborhoodProfiles.java
@@ -22,9 +22,8 @@ public class NeighborhoodProfiles {
     public static void main(String[] args) throws IOException {
         // TODO code application logic here
 
-        
-        if(args.length<3){
-            System.out.println("input: neighborhoodprofiles.jar k inputFile idNodo [buffer dimension]");
+        if (args.length < 3) {
+            System.out.println("input: neighborhoodprofiles.jar k inputFile idNodo [d/u] [buffer dimension]");
             return;
         }
 
@@ -32,14 +31,22 @@ public class NeighborhoodProfiles {
         Integer k = new Integer(args[0]);
         String inputFile = args[1];//"dataset/p2p-Gnutella3.txt";
         String nNodo = args[2];
-        
+        String graphType = "d";
+
         Integer bufDim = -1;
-        if(args.length >= 4 && args[3] != "")
-            bufDim = new Integer(args[3]);
-        
+
+        if (args.length >= 4 && args[4] != "") {
+            graphType = args[4];
+        }
+
+        if (args.length >= 5 && args[5] != "") {
+            bufDim = new Integer(args[5]);
+        }
+
         //all Neighborhood profiles
-        if(k < 0)
+        if (k < 0) {
             k = Integer.MAX_VALUE;
+        }
 
         //neigborhood profile |N(v,i)|
         HashSet<String> kset = new HashSet();
@@ -47,31 +54,34 @@ public class NeighborhoodProfiles {
         //add nodo da analizzare
         kset.add(nNodo);
         visited.add(nNodo);
-        
+
         System.out.println("Neighborhood profiles |N(v,0)|\t1");
-        
+
         //foreach step
         for (int i = 0; i < k; i++) {
 
-            System.out.print("Neighborhood profiles |N(v," + (i+1) + ")|\t");
+            System.out.print("Neighborhood profiles |N(v," + (i + 1) + ")|\t");
 
             //  performance step
-            kset = step.perform(kset, visited, inputFile,bufDim);
+            if (graphType.equals('d')) {
+                kset = step.performDirectGraph(kset, visited, inputFile, bufDim);
+            } else {
+                kset = step.performUndirectGraph(kset, visited, inputFile, bufDim);
+            }
 
             //stampa dimensione step
             System.out.print(kset.size() + "\n");
-            
-            if(kset.size() < 1){
+
+            if (kset.size() < 1) {
                 System.out.println("No more neighborhood --> stop processing");
                 return;
             }
-                
+
             //Stampa elementi del set
             //  print kset --> |N(v,i)|
 //            for(String s: kset){
 //                System.out.println(s+"\t");
 //            }
-
         }
     }
 
